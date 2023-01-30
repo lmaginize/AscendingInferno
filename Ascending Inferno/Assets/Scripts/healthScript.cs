5521f8ccb2242e9222e096300d10d1adb614bee2
BrianHBones <bhartwig123@gmail.com>
2023-01-30 09:48:53 -0600

    health script updates
    health looks like a fake word now

--- Ascending Inferno/Assets/Scripts/healthScript.cs
+++ Ascending Inferno/Assets/Scripts/healthScript.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class healthScript : MonoBehaviour
 {
     public int health = 3;
+    int maxHealth;
+    gameObject Player;
 
     void start(){
+        Player = GameObject.FindGameObjectWithTag("Player");
+        maxHealth == health;
+    }
 
+    void update(){
+    
     }
 
     public void takeDamage(){
         health--;
+    }
+
+    public void IncreaseHealth(int value){
+        if(health < maxHealth){
+            health += value;
+        }
+    }
+
+    public void die(){
+        if(health <= 0 && Player){
+            debug.log("DEAD!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        }
     }
 
     public int returnHealth(){
