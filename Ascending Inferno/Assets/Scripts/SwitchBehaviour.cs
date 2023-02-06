62647b737911db03d5562e497dfdd853c56552f3
Ben Lea <blea@mail.bradley.edu>
2023-02-05 18:58:33 -0600

    Door Switch and door

---
+++ Ascending Inferno/Assets/Scripts/SwitchBehaviour.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchBehaviour : MonoBehaviour
+{
+    public int switchType = 0;
+    public GameObject Door;
+    public GameObject Platform;
+    public GameObject Lava;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.Equals(player))
+        {
+            if (switchType == 0)
+            {
+                Door.SetActive(false);
+            }
+            
+        }
+    }
+}
