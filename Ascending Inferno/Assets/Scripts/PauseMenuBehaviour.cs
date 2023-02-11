88771b9f2d892bf3c6b55589ae4389cb0167aa4b
0Moochoo <wescamillamorque@mail.bradley.edu>
2023-02-11 14:34:44 -0600

    temp options menu

--- Ascending Inferno/Assets/Scripts/PauseMenuBehaviour.cs
+++ Ascending Inferno/Assets/Scripts/PauseMenuBehaviour.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using TMPro;
+using UnityEngine.UI;
 public class PauseMenuBehaviour : MonoBehaviour
 {
     //vars
     public static bool isPaused;
-
+    bool isInPauseMenu;
     //Gets a reference to the pause menu
     [SerializeField] GameObject pauseMenuUI;
+    [SerializeField] TMP_Text mouseValue, audioValue;
+    [SerializeField] Slider mouseSlider, audioSlider;
 
     private void Start()
     {
@@ -23,7 +26,7 @@ public class PauseMenuBehaviour : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape) && !isInPauseMenu)
         {
             //Checks if game is already paused
             if (isPaused)
@@ -45,6 +48,7 @@ public class PauseMenuBehaviour : MonoBehaviour
         pauseMenuUI.SetActive(false);
         isPaused = false;
         Time.timeScale = 1f;
+       
     }
 
     /// <summary>
@@ -52,9 +56,20 @@ public class PauseMenuBehaviour : MonoBehaviour
     /// </summary>
     private void Pause()
     {
-       
         pauseMenuUI.SetActive(true);
+        isInPauseMenu = true;
         isPaused = true;
         Time.timeScale = 0f;
     }
+
+    public void AudioSlider()
+    {
+        audioValue.text = audioSlider.value.ToString("F0");
+    }
+
+    public void MouseSlider()
+    {
+        mouseValue.text = mouseSlider.value.ToString("F0");
+    }
 }
+
