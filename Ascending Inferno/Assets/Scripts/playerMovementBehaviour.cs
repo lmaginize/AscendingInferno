d7879eac55926e45c19f85151ede9ae48c7c3307
BrianHBones <bhartwig123@gmail.com>
2023-02-14 21:53:07 -0600

    health max on checkpoint

--- Ascending Inferno/Assets/Scripts/playerMovementBehaviour.cs
+++ Ascending Inferno/Assets/Scripts/playerMovementBehaviour.cs
@@ -462,6 +462,15 @@ public class playerMovementBehaviour : MonoBehaviour
         }
     }
 
+    public void checkHealthed()
+    {
+        if (health < 3)
+        {
+            health = 3;
+            gc.UpdateHealthUI();
+        }
+    }
+
     public void PointChecked()
     {
         startingLocation.position = gameObject.transform.position;
